0228b9dd2726a96b6625d8c0ad1da4523d7a5640
SudharsanDeepak <sudharsandeepak12@gmail.com>
2026-02-22 00:53:02 +0530

    Add Vercel frontend URL to CORS configuration

--- AIML Project - Server/index.js
+++ AIML Project - Server/index.js
@@ -16,6 +16,7 @@ app.use(cors({
     'http://localhost:5173',
     'http://127.0.0.1:3000',
     'http://127.0.0.1:5173',
+    'https://cropintel-hub.vercel.app',
     process.env.FRONTEND_URL
   ].filter(Boolean),
   credentials: true,

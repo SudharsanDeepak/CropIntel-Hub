0031758b9b097b3c9e543aef1df8bbc10c12b8ae
SudharsanDeepak <sudharsandeepak12@gmail.com>
2026-02-22 12:46:18 +0530

    Add vite config

---
+++ AIML Project - Website/vite.config.js
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/utils/testSetup.js',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      exclude: [
+        'node_modules/',
+        'src/utils/testSetup.js',
+        'src/utils/mockData.js',
+        '**/*.test.jsx',
+        '**/*.properties.test.js'
+      ],
+      thresholds: {
+        lines: 80,
+        functions: 80,
+        branches: 80,
+        statements: 80
+      }
+    }
+  }
+})
